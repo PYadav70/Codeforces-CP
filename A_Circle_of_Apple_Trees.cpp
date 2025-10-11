42aedcbcda91ad0e21d7b3d4fcf7c378b051c12a
PYadav70 <technoyadav1234@gmail.com>
2025-10-12 00:35:11 +0530

    Implement solutions for Codeforces problems A. Goals of Victory and A. How Much Does Daytona Cost, including problem statements and test cases.

--- A_Circle_of_Apple_Trees.cpp
+++ A_Circle_of_Apple_Trees.cpp
@@ -9,30 +9,15 @@ int main()
     {
         int n;
         cin >> n;
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
+       
+        set<int>st;
+        for(int i=0; i<n; i++){
+            int x ;
+            cin >> x;
+            st.insert(x);
         }
-        int maxAppleEat = 1;
-
-        for (int start = 0; start < n; start++)
-        {
-            int eaten = 1;
-            int lastBeauty = arr[start];
-
-            for (int step = 1; step < n * n; step++)
-            {
-                int nextTree = (start + step) % n;
-                if (arr[nextTree] > lastBeauty)
-                {
-                    eaten++;
-                    lastBeauty = arr[nextTree];
-                }
-            }
-            maxAppleEat = max(maxAppleEat, eaten);
-        }
-        cout << maxAppleEat << endl;
+        int ans = st.size();
+        cout << ans << endl;
     }
     return 0;
 }
